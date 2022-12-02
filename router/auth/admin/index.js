0b7bb1f54ac552520ba52b7831c126a7be41b7db
ThisIsMeAdityaSingh <thisismeadityasingh99@gmail.com>
2022-12-02 22:37:57 +0530

    ToBE: admin routers and controllers

--- router/auth/admin/index.js
+++ router/auth/admin/index.js
@@ -1,3 +1,8 @@
 const authRouter = require('express').Router();
 
+// TODO: Admin Sign Up
+// TODO: Admin Sign in
+// TODO: Admin profile Update
+// TODO: Admin account delete
+
 module.exports = { authRouter };
